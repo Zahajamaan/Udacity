b89ef928b0fe6d7d9c853add69bbd6b55424de1a
Zahajamaan <zahajamaan@gmail.com>
2018-11-18 22:01:23 +0300

    Update reporting_tool.py

--- reporting_tool.py
+++ reporting_tool.py
@@ -1,16 +1,19 @@
+#!/usr/bin/env python3
+
 import psycopg2
 
 
 conn = psycopg2.connect("dbname=news")
 cursor = conn.cursor()
 
-def reporting_tool (cur,query, text , format):
+
+def reporting_tool(cur, query, text, format):
     cur.execute(query)
     rows = cursor.fetchall()
     print(text)
 
     for row in rows:
-        print( format % (row[0], row[1]))
+        print(format % (row[0], row[1]))
 
 
 reporting_tool(
@@ -27,13 +30,15 @@ reporting_tool(
     format=" '%s' - %s views"
 )
 
-#https://stackoverflow.com/a/15754763
+# https://stackoverflow.com/a/15754763
 
-reporting_tool(cur = cursor,
+reporting_tool(
+    cur=cursor,
     query="""
     SELECT authors.name ,COUNT(log.ip) as hits
     FROM articles, log, authors
-    WHERE authors.id = articles.author and log.path = CONCAT('/article/', articles.slug )
+    WHERE authors.id = articles.author \
+    and log.path = CONCAT('/article/', articles.slug )
     GROUP BY authors.name
     ORDER BY hits DESC
     LIMIT 4
@@ -43,18 +48,20 @@ reporting_tool(cur = cursor,
 )
 
 cursor.execute("""
-        SELECT DATE(time) as thedate ,CAST(count(status) AS INT) as errors,CAST(count(ip) AS INT) as hits
+        SELECT DATE(time) as thedate ,CAST(count(status) AS INT) as \
+        errors,CAST(count(ip) AS INT) as hits
         FROM log
         WHERE  status =  '404 NOT FOUND'
         GROUP BY thedate
-        HAVING ROUND( CAST(count(status) AS INT) / CAST(count(ip) AS INT)  * 100 ) > 1.00
+        HAVING \
+        ROUND(CAST(count(status) AS INT) / CAST(count(ip) AS INT)  * 100) > 1.0
         ORDER BY errors DESC
         LIMIT 1
   """)
 rows = cursor.fetchall()
 print ('3. On which days did more than 1% of requests lead to errors?')
 for row in rows:
-    print ( row[0].strftime('%B %d, %Y') + " — 1% errors" )
+    print (row[0].strftime('%B %d, %Y') + " — 1% errors")
 
 
 conn.close()
